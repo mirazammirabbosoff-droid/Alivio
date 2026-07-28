e0064f9fa58c18762c22201fdc2ee490deaf8a2d
mirazam-droid <mirazammirabbosoff@gmail.com>
2026-07-28 17:07:57 +0500

    added function

--- main.js
+++ main.js
@@ -38,3 +38,22 @@ function OnClickButton() {
     alert('Вы нажали Отмена');
   }
 }
+
+const moodSelect = document.getElementById('mood-select');
+
+    // Прослушивание изменения выбора в выпадающем списке
+    moodSelect.addEventListener('change', function() {
+      const selectedMood = moodSelect.value;
+
+      if (selectedMood === 'sad') {
+        alert('just be happy bro');
+      } else if (selectedMood === 'happy') {
+        alert('keep smiling bro!');
+      } else if (selectedMood === 'angry') {
+        alert('calm down bro, take a deep breath');
+      } else if (selectedMood === 'tired') {
+        alert('get some rest bro!');
+      } else if (selectedMood === 'kind') {
+        alert('you are awesome bro!');
+      }
+    });
